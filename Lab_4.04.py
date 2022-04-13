cc3cfbed9bbaacff3318daec00aeca2b2bb84bfc
Martin <mwgoff24@students.bmchs.org>
2022-04-13 10:11:27 -0500

    worked more

--- Lab_4.04.py
+++ Lab_4.04.py
@@ -95,41 +95,100 @@ Second to last element: my_list[-2]
 Third to last element: my_list[-3]
 '''
 # shopping lists
-from ast import ListComp
-
 
 shopping_lists = [
     ['toothpaste', 'q-tips', 'milk'],
     ['milk', 'candy', 'apples'],
     ['planner', 'pencils', 'q-tips']
 ]
-# inputs
+'''
+Different way to print out items in a list
 for list in shopping_lists:
     for item in list:
         if item == 'q-tips':
             print(item)
 
-# for i in range(len(shopping_lists)):
-#     for j in range(len(shopping_lists[i])):
-#         print(shopping_lists[i][j])
+for i in range(len(shopping_lists)):
+    for j in range(len(shopping_lists[i])):
+        print(shopping_lists[i][j])
+'''
 
-def update_list():
-    pass
+# functions
+# def update_list(list_num, item_num, new_item):
+#     shopping_lists[int(list_num)-1][int(item_num)-1] = new_item
+#     print(shopping_lists)
 
-def print_item():
-    list_choice = input("What list would you like to choose, 1, 2, or 3? ")
-    item_choice = input("What item would you like to choose, 1, 2, or 3? ")
-    print(shopping_lists[int(list_choice)-1][int(item_choice)-1])
+# def print_item(list_num, item_num):
+#     print(shopping_lists[int(list_num)-1][int(item_num)-1])
     
 
-def print_list():
-    list_choice = input("What list would you like to choose, 1, 2, or 3? ")
-    print(shopping_lists[int(list_choice)-1])
+# def print_list(list_num):
+#     print(shopping_lists[int(list_num)-1])
 
-choice = input("Choose either: 1 = update item, 2 = view item,  3 = view list: ")
-if choice == '1':
-    update_list()
-elif choice == '2':
-    print_item()
-elif choice == '3':
-    print_list()
+# # inputs
+# choice = input("Choose either: 1 = update item, 2 = view item,  3 = view list: ")
+# if choice == '1':
+#     list_choice = input("What list number, 1, 2, or 3, contains the item you would like to update? ")
+#     item_choice = input("What item number would you like to update? ")
+#     new_item = input("What item do you want to add? ")
+#     update_list(list_choice, item_choice, new_item)
+# elif choice == '2':
+#     list_choice = input("What list would you like to view, 1, 2, or 3? ")
+#     item_choice = input("What item would you like to view from this list, 1, 2, or 3? ")
+#     print_item(list_choice, item_choice)
+# elif choice == '3':
+#     list_choice = input("What list would you like to view, 1, 2, or 3? ")
+#     print_list(list_choice)
+
+
+# part 2
+
+# all in one, combines all lists into one
+def all_in_one():
+    new_list = []
+    for list in shopping_lists:
+        for item in list:
+            new_list.append(item)
+    print(new_list)
+
+all_in_one()
+
+# count q-tips, displays a count of how many q-tips are in shopping_lists
+def count_qtips():
+    qtip_count = 0
+    for list in shopping_lists:
+        for item in list:
+            if item == 'q-tips':
+                qtip_count += 1
+    print(f"You have {qtip_count} q-tips.")
+
+count_qtips()
+
+# drink more milk, adds milk to lists that don't already contain it
+def drink_more_milk():
+    for list in shopping_lists:
+        if 'milk' not in list:
+            list.append('milk')
+    print(shopping_lists)
+
+drink_more_milk()
+
+# if you give a mouse a cookie, adds cookies to lists with milk in them
+def if_you_give_a_mouse_a_cookie():
+    for list in shopping_lists:
+        if 'milk' in list:
+            list.append('cookies')
+    print(shopping_lists)
+
+if_you_give_a_mouse_a_cookie()
+
+# extension
+
+# reverse all, reverses lists and items in lists
+shopping_cart = [
+    ['q-tips', 'pencils', 'planner'],
+    ['apples', 'candy', 'milk'],
+    ['milk', 'q-tips', 'tooth paste']
+]
+def reverse_all():
+    pass
